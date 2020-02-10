cdbd6d1106cdef997ff7c439d6d5ca76637fbb04
Unknown <seguragabriel@unbosque.edu.co>
2020-02-10 16:09:42 -0500

    WIP feb 10

---
+++ Menu/menu.js
@@ -0,0 +1,58 @@
+// header images
+$('#banner-logo').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/diplomado-investigacion-clinica-amgen/images/assets/logo-amgen-white.svg" alt="AMGEN"/>');
+
+$('#banner-image').html('<img class="imagen" src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/diplomado-investigacion-clinica-amgen/images/assets/banner-diplomado.jpg" alt="Diplomado virtual de investigación clínica"/>');
+
+
+
+
+// Evaluaciones modulos
+$('#test-mod-1').html('<a class="white-btn" href="#">Evaluación &rsaquo;</a><a class="white-btn" href="#" target="_blank">Caso de Estudio &rsaquo;</a><a class="white-btn" href="#" target="_blank">Videoconferencia &rsaquo;</a>');
+
+// Menu copy
+//$('#menu').html('<div id="menu_curso"> <div id="menuanchor-top"> <nav> <div class="menu-toggle"> <i class="fa fa-bars"> </i> Menú </div><ul> <li class="dropdown timeit" data-start="2020-02-10 00:00"> <a href="https://ubosquemoodle.unbosque.edu.co/course/view.php?id=4575" class="no-arrows"> Inicio </a></li><li class="dropdown timeit no-activities" data-start="2019-07-27 00:00"> <a href="#"> Lineamientos </a> <ul class="dropdown-menu"> <li> <a href="#">Enlaces </a> </li></ul> </li><li class="dropdown timeit" data-start="2020-02-10 00:00"> <a href="#"> Módulo 1 </a> <ul class="dropdown-menu"> <li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9673">Generalidades de epidemiología e investigación clínica </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9674">Introducción a la epidemiología e investigación clínica </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9675">¿Cómo se realiza la concepción y formulación de la pregunta de investigación? </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9676">¿Cómo se desarrolla la pregunta de investigación? </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9677">¿Cómo se realiza la elección de los participantes en un estudio clínico? </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9678">¿Cuáles son los tipos de muestreo en investigación clínica? </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9679">¿Cómo se realiza la planificación de las mediciones? precisión y exactitud </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9680">Sesgo de selección </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9681">Sesgo de información </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9682">Confusión </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9683">¿Cómo se determina la causalidad y validez en investigación? </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9684">¿Cuáles son los elementos básicos de una propuesta de investigación clínica? </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9685">Bibliografía </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9686">Glosario </a> </li></ul> </li><li class="dropdown timeit" data-start="2020-03-27 00:00"> <a href="#"> Módulo 2 </a> <ul class="dropdown-menu"> <li> <a href="#">Enlaces </a> </li></ul> </li></ul> </nav> </div></div>');
+
+
+// footer curso amgen #1
+//$('#footer').html('<div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/diplomado-herramientas-para-la-gestion-socioambiental-en-la-industria-del-gas/images/svg/logo-ueb.svg" alt="Universidad El Bosque"/></div><div><span>© Todos los derechos reservados</span></div>');
+
+
+//
+// barras progreso
+//
+// progreso 10%
+$('#progreso10').html('<div class="progress progress-striped active"><div class="bar" style="width: 10%;"></div></div>');
+// progreso 20%
+$('#progreso20').html('<div class="progress progress-striped active"><div class="bar" style="width: 20%;"></div></div>');
+// progreso 30%
+$('#progreso30').html('<div class="progress progress-striped active"><div class="bar" style="width: 30%;"></div></div>');
+// progreso 40%
+$('#progreso40').html('<div class="progress progress-striped active"><div class="bar" style="width: 40%;"></div></div>');
+// progreso 50%
+$('#progreso50').html('<div class="progress progress-striped active"><div class="bar" style="width: 50%;"></div></div>');
+// progreso 60%
+$('#progreso60').html('<div class="progress progress-striped active"><div class="bar" style="width: 60%;"></div></div>');
+// progreso 70%
+$('#progreso70').html('<div class="progress progress-striped active"><div class="bar" style="width: 70%;"></div></div>');
+// progreso 80%
+$('#progreso80').html('<div class="progress progress-striped active"><div class="bar" style="width: 80%;"></div></div>');
+// progreso 90%
+$('#progreso90').html('<div class="progress progress-striped active"><div class="bar" style="width: 90%;"></div></div>');
+// progreso 100%
+$('#progreso100').html('<div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div>');
+
+$('#material-mod-1').html('<div class="wow fadeInUp material-modulo"> <h2>Material módulo </h2> <div class="materiales"> <div class="elementos"> <div> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/diplomado-investigacion-clinica-amgen/images/mod-1/pdf01.jpg"/> </div><p class="italic">Presentación PPT Módulo 1 Generalidades epidemiológicas e investigación clínica</p><a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9687" class="white-btn">Ver más <i class="fas fa-external-link-alt"></i> </a> </div><div class="elementos"> <div> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/diplomado-investigacion-clinica-amgen/images/mod-1/pdf02.jpg"/> </div><p class="italic">La pregunta de investigación en la práctica clínica: guía para formularla</p><a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9688" class="white-btn">Ver más <i class="fas fa-external-link-alt"></i> </a> </div><div class="elementos"> <div> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/diplomado-investigacion-clinica-amgen/images/mod-1/pdf03.jpg"/> </div><p class="italic">Primeros pasos en la confección de un proyecto de investigación clínica: la pregunta de investigación</p><a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9689" class="white-btn">Ver más <i class="fas fa-external-link-alt"></i> </a> </div><div class="elementos"> <div> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/diplomado-investigacion-clinica-amgen/images/mod-1/pdf04.jpg"/> </div><p class="italic">Methods to improve recruitment to randomised controlled trials: Cochrane systematic review and meta-analysis</p><a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=340011&chapterid=9690" class="white-btn">Ver más <i class="fas fa-external-link-alt"></i> </a> </div></div></div>');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
